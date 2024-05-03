853d6ca429cfd8009a3cb8354d52e5dbb4945d7c
unknown <mylenarayahne11@gmail.com>
2024-05-03 18:16:02 -0300

    add spring na  branch

---
+++ sistemaBradescoDemanda/src/main/java/com/bradesco/sistemaBradescoDemanda/SistemaBradescoDemandaApplication.java
@@ -0,0 +1,13 @@
+package com.bradesco.sistemaBradescoDemanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaBradescoDemandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaBradescoDemandaApplication.class, args);
+	}
+
+}
---
+++ sistemaBradescoDemanda/src/test/java/com/bradesco/sistemaBradescoDemanda/SistemaBradescoDemandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bradesco.sistemaBradescoDemanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaBradescoDemandaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
